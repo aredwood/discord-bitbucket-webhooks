b55f68859b80041f2d9e442da00e46a34889cea5
Alex Redwood <hello@alexredwood.com>
2018-05-01 01:44:08 +1000

    dont know how useful this is going to be.

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
+let rp = require("request-promise");
 //who knows tbh
-module.exports = {
-    config:true
+let config = {
+    config:true,
+    external: () => {
+        return rp("https://canihazip.com/s").then(response => {
+            return response
+        }).catch(err => {
+            console.log("failed to get external IP address");
+            return null;
+        })
+    }
 };
+
+
+
+module.exports = config;
